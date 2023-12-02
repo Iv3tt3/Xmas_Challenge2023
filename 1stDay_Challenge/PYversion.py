e17df0ca724456ea814c9153590807398cdcf11d
Iv3tt3 <reach.iv3tt3@gmail.com>
2023-12-02 19:29:52 +0000

    1st challenge - Python tests and  method repeted_first

--- 1stDay_Challenge/PYversion.py
+++ 1stDay_Challenge/PYversion.py
@@ -1,3 +1,12 @@
 class Gifts():
     def __init__(self, ids):
         self.ids = ids
+
+    # Return first id is repeted in the following ids of the list
+    def repeted_first(self):
+        result = -1
+        for i in range(len(self.ids)):
+            num = self.ids[0]
+            if num in self.ids[i+1:]:
+                return num
+        return result
--- 1stDay_Challenge/test_PYversion.py
+++ 1stDay_Challenge/test_PYversion.py
@@ -12,3 +12,17 @@ def test_create_class():
     ids2 = [5, 1, 5, 1]
     gifts2 = Gifts(ids2)
     assert gifts2.ids == ids2
+
+def test_repeted_first():
+    ids = [2, 1, 3, 5, 3, 2]
+    gifts = Gifts(ids)
+    assert gifts.repeted_first() == 2
+
+    ids1 = [1, 2, 3, 4]
+    gifts1 = Gifts(ids1)
+    assert gifts1.repeted_first() == -1
+
+    ids2 = [5, 1, 5, 1]
+    gifts2 = Gifts(ids2)
+    assert gifts2.repeted_first() == 5
+
